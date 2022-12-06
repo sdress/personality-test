1b3d97125eca856770c029af4b9cd47a812f3e27
sdress <sarah.dresser1@gmail.com>
2022-12-06 22:31:19 +0100

    fixing 404 error

--- flask_app/controllers/routes.py
+++ flask_app/controllers/routes.py
@@ -6,6 +6,6 @@ from flask import render_template, redirect, request, session, flash
 # bcrypt = Bcrypt(app)
 
 @app.route('/')
-def show_home_page():
-    return render_template('index.html')
-    # return f"Hello World"
+def show_home():
+    # return render_template('index.html')
+    return "Hello World"
